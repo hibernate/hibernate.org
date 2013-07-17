27ab8ddbd6354061873273948c3ad8f8d86f5df5
Emmanuel Bernard <emmanuel@hibernate.org>
2013-07-17 20:06:11 +0200

    Remove Less files and dependency and add Sass shell

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,7 +3,6 @@ require 'js_minifier'
 require 'css_minifier'
 require 'html_minifier'
 require 'file_merger'
-require 'less_config'
 
 Awestruct::Extensions::Pipeline.new do
   development = Engine.instance.site.profile == 'development'
@@ -19,7 +18,6 @@ Awestruct::Extensions::Pipeline.new do
   transformer Awestruct::Extensions::CssMinifier.new
   transformer Awestruct::Extensions::HtmlMinifier.new
   extension Awestruct::Extensions::FileMerger.new
-  extension Awestruct::Extensions::LessConfig.new
   extension Awestruct::Extensions::Indexifier.new
 end
 
