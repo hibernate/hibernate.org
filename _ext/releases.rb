0fb01dbd3c6b7bce72d248715c48e730217510b6
Gunnar Morling <gunnar.morling@googlemail.com>
2013-08-30 15:26:35 +0200

    #22 Making sure project pages can be displayed also if there is no release or "Getting started" URL

--- _ext/releases.rb
+++ _ext/releases.rb
@@ -4,24 +4,34 @@ module Awestruct
 
       def latest_stable_release(p = page)
         releases = site.projects[p.project].releases
+
+        if releases.nil?
+          return nil
+        end
+
         # releases are ordered by latest first in the metadata
         releases.each do |release|
           if release.stable
             return release
           end
         end
-        return Nil
+        return nil
       end
 
       def latest_dev_release(p = page)
         releases = site.projects[p.project].releases
+
+        if releases.nil?
+          return nil
+        end
+
         # releases are ordered by latest first in the metadata
         releases.each do |release|
           if not release.stable
             return release
           end
         end
-        return Nil
+        return nil
       end
     end
   end
