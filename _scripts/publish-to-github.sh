ac85e15538c92937b2d5765a2e5a537805e40335
Emmanuel Bernard <emmanuel@hibernate.org>
2013-11-28 23:32:56 +0100

    WEBSITE-82 Add script to build and publish the site to github pages

---
+++ _scripts/publish-to-github.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# build site in prod profile
+# clone hibernate.github.io in _tmp if not present
+# copy site to git repo, commit and push
+rake clean gen[production]
+cd _tmp
+if [ ! -d "hibernate.github.io" ];
+then
+  git clone git@github.com:hibernate/hibernate.github.io.git
+fi
+cd hibernate.github.io
+git fetch origin
+git reset --hard origin/master
+git rm -r .
+
+rsync -av \
+      ../../_site/ .
+
+git add .
+git commit -m "Publish generated site"
+git push origin master
+cd ../..
