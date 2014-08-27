e41a1b7444fbf19106b790cf6710e3aacdd4a993
Hardy Ferentschik <hardy@ferentschik.de>
2014-08-27 12:57:25 +0200

    WEBSITE-213 Update rspec matcher syntax to Rspec 3

--- _spec/version_spec.rb
+++ _spec/version_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../_ext/release_file_parser'
- 
+
 describe Awestruct::Extensions::Version do
 
     before :all do
@@ -12,31 +12,31 @@ describe Awestruct::Extensions::Version do
             Awestruct::Extensions::Version.new("1.0.1.Final"),
             Awestruct::Extensions::Version.new("1.1.0.Alpha1"),
             Awestruct::Extensions::Version.new("2.0.0.Alpha1"),
-            Awestruct::Extensions::Version.new("5.1.2.Beta4"),            
+            Awestruct::Extensions::Version.new("5.1.2.Beta4"),
         ]
     end
 
     describe "#major" do
     	it "major number is correct" do
-        	@versions[8].major.should eql 5
+        	expect(@versions[8].major).to eql 5
     	end
 	end
 
 	describe "#feature_group" do
     	it "feature_group number is correct" do
-        	@versions[8].feature_group.should eql 1
+        	expect(@versions[8].feature_group).to eql 1
     	end
 	end
 
 	describe "#feature" do
     	it "feature number is correct" do
-        	@versions[8].feature.should eql 2
+        	expect(@versions[8].feature).to eql 2
     	end
 	end
 
 	describe "#bugfix" do
     	it "bugfix number is correct" do
-        	@versions[8].bugfix.should eql "Beta4"
+        	expect(@versions[8].bugfix).to eql "Beta4"
     	end
 	end
 
@@ -44,8 +44,8 @@ describe Awestruct::Extensions::Version do
 		it "version array is in correct order" do
 			@versions.each_with_index do |version, index|
 				break if index == (@versions.length - 2)
-				(version <=> @versions[index + 1] ).should == -1
+				expect((version <=> @versions[index + 1] )).to eq -1
 			end
 		end
-	end 
+	end
 end
