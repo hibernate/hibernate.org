3f0338f08612264da067149ad93eae2a91e4089f
Emmanuel Bernard <emmanuel@hibernate.org>
2015-04-30 18:22:46 +0200

    Support old and new sourceforge URL scheme
    The Sourceforge URL header is explicitly set for Hibernate 4 releases
    now in the .yml files

--- _ext/releases.rb
+++ _ext/releases.rb
@@ -36,7 +36,13 @@ module Awestruct
 
       # Accepts a project description (YML) and a release (YML) and return the Sourceforge URL to the zip download
       def sourceforge_zip_url(project, release)
-        return "#{project.sourceforge_url}#{release.version}/#{project.zip_file}/download".gsub('VERSION', release.version)
+        # this if clause is not idiomatic to Ruby : TODO improve
+        if release.sourceforge_url.instance_of? String
+          sourceforge_url = release.sourceforge_url
+        else
+          sourceforge_url = project.sourceforge_url
+        end
+        return "#{sourceforge_url}#{release.version}/#{project.zip_file}/download".gsub('VERSION', release.version)
       end
     end
   end
