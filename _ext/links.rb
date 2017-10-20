e37ad6b6158fa396934c9d243ce4196f2f4a51e9
Yoann Rodière <yoann@hibernate.org>
2017-10-20 12:36:06 +0200

    Fix JIRA ticket links for ORM in the release list

--- _ext/links.rb
+++ _ext/links.rb
@@ -35,6 +35,27 @@ module Awestruct
         return "https://repository.jboss.org/nexus/index.html#nexus-search;gav~#{group_id_pattern}~#{artifact_id_pattern}~#{version_pattern}~~"
       end
 
+      def jira_issues_for_series_url(project, series)
+        versions = series.releases.collect{|r| r.version}
+        return jira_issues_for_versions_url(project, versions)
+      end
+
+      def jira_issues_for_release_url(project, release)
+        return jira_issues_for_versions_url(project, [release.version])
+      end
+
+      def jira_issues_for_versions_url(project, versions)
+        fix_version_translator = _fix_version_translator(project)
+        comma_separated_fix_versions = versions.collect{|v| fix_version_translator.call(v)}.join( "%2C%20" )
+        return "https://hibernate.atlassian.net/issues/?jql=project%20%3D%20#{project.jira['key']}%20AND%20fixVersion%20in%20(#{comma_separated_fix_versions})%20ORDER%20BY%20updated"
+      end
+
+      def _fix_version_translator(project)
+        return project.jira['key'] == 'HHH' ?
+          lambda {|v| v != '4.2.0.Final' && v != '4.3.0.Final' && v != '5.0.0.Final' && v =~ /^(.*).Final$/ ? $1 : v }
+          : lambda {|v| v}
+      end
+
       # Accepts a project description (YML) and a release (YML) and return the Sourceforge URL to the zip download
       def sourceforge_zip_url(project, release)
         # this if clause is not idiomatic to Ruby : TODO improve
