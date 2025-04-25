91f2100a130c7dd6cb0e88a3c1b26424b6c9f11d
Gavin King <gavin@hibernate.org>
2025-04-25 12:35:01 +0200

    add links to short guide

--- _ext/links.rb
+++ _ext/links.rb
@@ -24,6 +24,10 @@ module Awestruct
         return DocumentRef.from_patterns(project, series, :migration_guide)
       end
 
+      def short_guide(project, series)
+        return DocumentRef.from_patterns(project, series, :short_guide)
+      end
+
       def whats_new(project, series)
         return DocumentRef.from_patterns(project, series, :whats_new)
       end
