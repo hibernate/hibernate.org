5bbc0d7b5bf27ee3bf44499ac65a792785afb0e1
Yoann Rodière <yoann@hibernate.org>
2025-10-01 18:13:03 +0200

    Expose project metadata as JSON
    For consumption by e.g. Javascript on other websites.

--- _ext/release_file_parser.rb
+++ _ext/release_file_parser.rb
@@ -117,7 +117,6 @@ module Awestruct
           series = series.merge(subproject_series)
         end
 
-        series[:project] = project
         if ( series[:version] == nil )
           series[:version] = File.basename( series_dir )
         end
@@ -142,8 +141,6 @@ module Awestruct
           release = release.merge(subproject_release)
         end
 
-        release[:project] = project
-        release[:series] = series
         if ( release[:version] == nil )
           File.basename( release_file ) =~ /^(.*)\.\w*$/
           release[:version] = $1
