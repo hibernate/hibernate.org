f26074ca7644341d06fc547eec91c7a9f5113a7d
Yoann Rodière <yoann@hibernate.org>
2026-09-08 08:51:51 +0000

    Show cross-project compatibility in project release pages
    For every project series, automatically find other Hibernate projects
    that reference it in their integration_constraints and display them
    in the same compatibility matrices alongside external integrations.
    
    Ordering is centralized in a site-wide integration_display_order:
    libraries/specs first, then Hibernate projects, then downstream
    frameworks. Project version numbers link to their release pages.
    
    Also removes orm/search from integrations.yml since reverse
    compatibility is now computed automatically for all projects.

--- _ext/releases.rb
+++ _ext/releases.rb
@@ -32,13 +32,13 @@ module Awestruct
         return project.release_series[version]
       end
 
-      def integration_constraint(constraint)
+      def integration_constraint(constraint, link_prefix = nil)
         version = constraint[:version]
         if version.nil?
           return nil
         end
         if version.is_a?(Array)
-          rendered = version.map { |v| integration_constraint_version(v) }
+          rendered = version.map { |v| integration_constraint_version(v, true, link_prefix) }
           if rendered.length <= 1
             rendered.join
           elsif rendered.length == 2
@@ -47,20 +47,23 @@ module Awestruct
             rendered[0..-2].join(", ") + " or " + rendered[-1]
           end
         else
-          integration_constraint_version(version)
+          integration_constraint_version(version, true, link_prefix)
         end
       end
 
-      def integration_constraint_version(version, includeComment = true)
+      def integration_constraint_version(version, includeComment = true, link_prefix = nil)
         if version.is_a?(Hash)
           if version.key?(:from)
-            "#{integration_constraint_version(version.from, includeComment)}&nbsp;&rarr;&nbsp;#{integration_constraint_version(version.to, includeComment)}" +
-              (includeComment && version.key?(:comment) ? " (#{version.comment})" : "")
+            "#{integration_constraint_version(version[:from], includeComment, link_prefix)}&nbsp;&rarr;&nbsp;#{integration_constraint_version(version[:to], includeComment, link_prefix)}" +
+              (includeComment && version.key?(:comment) ? " (#{version[:comment]})" : "")
           else
-            version.value.to_s + (includeComment && version.key?(:comment) ? " (#{version.comment})" : "")
+            val = version[:value].to_s
+            rendered_val = link_prefix ? "link:#{link_prefix}#{val}/[#{val}]" : val
+            rendered_val + (includeComment && version.key?(:comment) ? " (#{version[:comment]})" : "")
           end
         else
-          version.to_s
+          val = version.to_s
+          link_prefix ? "link:#{link_prefix}#{val}/[#{val}]" : val
         end
       end
     end
